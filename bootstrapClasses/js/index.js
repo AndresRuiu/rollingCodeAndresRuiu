c4db942c675461bb3bf954ccb8184fc8e0a30e0b
AndresRuiu <andresruiu@gmail.com>
2023-03-23 18:19:16 -0300

    bootstrap

---
+++ bootstrapClasses/js/index.js
@@ -0,0 +1,10 @@
+function mostrarAlerta() {
+    var alerta = document.getElementsByClassName("alert")[0];
+    alerta.classList.remove("d-none");
+  }
+  
+  function ocultarAlerta() {
+    var alerta = document.getElementsByClassName("alert")[0];
+    alerta.classList.add("d-none");
+  }
+  
